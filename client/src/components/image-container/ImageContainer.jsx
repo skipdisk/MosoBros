e84e9682eae9b4d8f5f53acc0e8fcd9fee5922e1
Thien Nguyen <Thiennguyenwsb@gmail.com>
2020-03-17 20:43:37 -0700

    added blurring

--- client/src/components/image-container/ImageContainer.jsx
+++ client/src/components/image-container/ImageContainer.jsx
@@ -35,15 +35,6 @@ const ImageContainer = histograms => {
   const pictureRef = useRef(null)
   const prevBrightness = usePrevious(brightnessValue)
 
-  // const bin = [...Array(19).keys()];
-
-  useEffect(() => {
-    // Update the document title using the browser API
-    {
-      // console.log(histograms.length);
-    }
-  })
-
   const changeCanvasSize = imageUrl => {
     var img = new Image()
     img.onload = function () {
@@ -69,6 +60,68 @@ const ImageContainer = histograms => {
 
   const uploadImages = () => {
     dispatch(imageUpload(pictureRef.current.toDataURL()))
+  }
+
+  function blurringHelper (imageData, callback) {
+    const canvas = pictureRef.current
+    const ctx = canvas.getContext('2d')
+    let myImageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+    for (var i = 0; i < myImageData.data.length; i += 4) {
+      var r = myImageData.data[i]
+      var g = myImageData.data[i + 1]
+      var b = myImageData.data[i + 2]
+      var a = myImageData.data[i + 3]
+
+      var channels = callback(r, g, b, a, myImageData.data, i)
+
+      myImageData.data[i] = channels.r
+      myImageData.data[i + 1] = channels.g
+      myImageData.data[i + 2] = channels.b
+      myImageData.data[i + 3] = channels.a
+      //
+    }
+
+    ctx.putImageData(myImageData, 0, 0)
+  }
+
+  //Uses the gaussian blur on the pictures on a 3x3 area
+  function blurring () {
+    const canvas = pictureRef.current
+    const ctx = canvas.getContext('2d')
+    let myImageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+    const w = canvas.width * 4
+    // the offset index for each pixel excluding the center pixel
+    const grid = [-w - 4, -w, -w + 4, -4, 4, w - 4, w, w + 4]
+
+    blurringHelper(myImageData, (r, g, b, a, dat, i) => {
+      var idx, count
+      r *= r
+      g *= g
+      b *= b
+      count = 1
+      for (idx = 0; idx < grid.length; idx++) {
+        const off = grid[idx]
+        if (i + off >= 0 && i + off < w * canvas.height) {
+          r += dat[i + off] * dat[i + off]
+          g += dat[i + 1 + off] * dat[i + 1 + off]
+          b += dat[i + 2 + off] * dat[i + 2 + off]
+          a += dat[i + 3 + off]
+          count++
+        }
+      }
+      r = Math.sqrt(r / count)
+      g = Math.sqrt(g / count)
+      b = Math.sqrt(b / count)
+      a = a / count
+      return {
+        r,
+        g,
+        b,
+        a
+      }
+    })
   }
 
   const invert = () => {
@@ -162,6 +215,8 @@ const ImageContainer = histograms => {
       <Button onClick={uploadImages}>Submit</Button>
       <Button onClick={invert}>Invert</Button>
       <Button onClick={greyscale}>Grey Scale</Button>
+      <Button onClick={blurring}>Blurring</Button>
+
       <Slider
         value={brightnessValue}
         onChange={handleBrightnessChange}
